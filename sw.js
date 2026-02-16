55c55c75476dea2c661fc839a0b2a826bae2465a
jalallelrumii-png <jalallelrumii@gmail.com>
2026-02-16 07:36:14 +0700

    Add service worker for caching static assets

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = 'nikah-easy-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+// Install Service Worker & simpan aset ke cache
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
+
+// Ambil aset dari cache jika offline
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
+
